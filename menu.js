b235080962a214c93ede1b8db87b00e9a82176f5
ubesha fatima <thedigitalmarketing64@gmail.com>
2024-10-04 17:00:01 +0500

    Add files via upload

--- menu.js
+++ menu.js
@@ -11,3 +11,26 @@ function submitSelection() {
    }
 }
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const hamburgerBtn = document.getElementById('hamburgerBtn');
+const navLinks = document.getElementById('navLinks');
+
+hamburgerBtn.addEventListener('click', function() {
+    navLinks.classList.toggle('show'); // Toggle the 'show' class
+});
